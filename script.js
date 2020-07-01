2001b5f8b66268cc2d6aa10f4556a16e22d40a47
DashaDidyk <dashka.didyk@gmail.com>
2020-07-02 00:29:09 +0300

    done

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 
 // 1. Закончить задачку из классной работы 
 
-// let userAge = +prompt('How old are you?');
-//     if (isNaN(userAge)) {
-//     alert('Enter the number');
-// }   else if (userAge >= 18) {
-//     alert('Welcome');
-// }   else if (userAge < 12) {
-//     alert('Use another service!!!');
-// }   else {
-//     alert('Access is denied');
-// }
+let userAge = +prompt('How old are you?');
+    if (isNaN(userAge)) {
+    alert('Enter the number');
+}   else if (userAge >= 18) {
+    alert('Welcome');
+}   else if (userAge < 12) {
+    alert('Use another service!!!');
+}   else {
+    alert('Access is denied');
+}
 
 
-// let userAge = +prompt('How old are you?');
-//     console.log('userAge', userAge);
-//     (isNaN(userAge)) ? alert('Enter the number') :
-//     (userAge >= 18) ? alert('Welcome') :
-//     (userAge <= 12) ? alert('Use another service!!!') : 
-//     alert('Access is denied'); 
+let ageUser = +prompt('How old are you?');
+    console.log('ageUser', ageUser);
+    (isNaN(ageUser)) ? alert('Enter the number') :
+    (ageUser >= 18) ? alert('Welcome') :
+    (ageUser <= 12) ? alert('Use another service!!!') : 
+    alert('Access is denied'); 
 
 
 // 2. Написать опросник для поступления на работу. К примеру, захардкоженные условия требуют, чтобы человек был не младше 30 лет, имел опыт в данной области больше 3-х лет и т.п. Выдать результат: подходит человек на должность, или нет.
@@ -48,7 +48,7 @@ switch (education) {
 }
 console.log(age, workExperience, hobby, education);
 
-if ((age > 17 && age < 32) && workExperience >= 2 && hobby !== 'нет' && education === "да") {
+if ((age > 17 && age < 32) && workExperience >= 2 && hobby !== 'нет' || education === "да") {
     alert('Добро пожаловать на работу');
 } else {
     alert("Увы, вы нам не подходите");
